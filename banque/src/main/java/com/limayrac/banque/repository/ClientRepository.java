933a088e9cadd3cffc8540cdd4a40ef329aa1f74
Jules Broto <77014034+JuleSpace@users.noreply.github.com>
2024-04-02 20:36:14 +0200

    Delete banque directory

--- banque/src/main/java/com/limayrac/banque/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.limayrac.banque.repository;
-
-import com.limayrac.banque.model.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepository extends CrudRepository<Client, Integer> {
-}
--- banque/src/main/java/com/limayrac/banque/repository/CompteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.limayrac.banque.repository;
-
-import com.limayrac.banque.model.Compte;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompteRepository extends CrudRepository<Compte, Integer> {
-}
